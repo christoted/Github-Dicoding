cac92d611f2a04c6643ab146b55ed8a715039104
Christopher Teddy <vitedocorp@yahoo.com>
2021-01-01 15:07:43 +0700

    Submission 3 add Photo

--- app/src/main/java/com/example/githubuser/adapter/FollowFollowerAdapter.kt
+++ app/src/main/java/com/example/githubuser/adapter/FollowFollowerAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.githubuser.R
 import com.example.githubuser.model.GithubFollowerItem
 import kotlinx.android.synthetic.main.item_following.view.*
@@ -21,6 +22,9 @@ class FollowFollowerAdapter(
 
         fun bind(followerItem: GithubFollowerItem) {
             itemView.id_tv_item_following_name.text = followerItem.login
+            Glide.with(context)
+                .load(followerItem.avatar_url)
+                .into(itemView.id_iv_item_following)
         }
     }
 
